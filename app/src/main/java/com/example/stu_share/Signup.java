0db3190efca464ad957481b4fea2d9ea686749f2
yushi-code <shy1860@hotmail.com>
2019-11-02 08:22:41 -0400

    add dharam

--- app/src/main/java/com/example/stu_share/Signup.java
+++ app/src/main/java/com/example/stu_share/Signup.java
@@ -1,17 +1,11 @@
 package com.example.stu_share;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Signup extends AppCompatActivity {
     private Button btnNext;
--- app/src/main/java/com/example/stu_share/Signup2.java
+++ app/src/main/java/com/example/stu_share/Signup2.java
@@ -1,8 +1,8 @@
 package com.example.stu_share;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Signup2 extends AppCompatActivity {
 
