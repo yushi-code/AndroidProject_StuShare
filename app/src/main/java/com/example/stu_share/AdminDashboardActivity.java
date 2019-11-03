d15e0f3b78ff86fa080e1ea64d53cc348d9b5aa7
Ayusha Chaudhry <ayusha.chaudhry@georgebrown.ca>
2019-11-03 12:30:02 -0500

    added logout functionality for admin dashboard

--- app/src/main/java/com/example/stu_share/AdminDashboardActivity.java
+++ app/src/main/java/com/example/stu_share/AdminDashboardActivity.java
@@ -8,12 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class AdminDashboardActivity extends AppCompatActivity {
-    Button profile,viewUser,viewEvents;
+    Button profile,viewUser,viewEvents, btnLogout;
     public static User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        btnLogout = findViewById(R.id.btnAdminLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
         user=(User)getIntent().getSerializableExtra("user");
         profile = findViewById(R.id.btnAdmProfile);
         viewUser=findViewById(R.id.btnViewUsers);
@@ -42,6 +49,10 @@ public class AdminDashboardActivity extends AppCompatActivity {
     public void openMyProfileEvent(){
         Intent intent =new Intent(this, MyProfile.class);
         intent.putExtra("user",user);
+        startActivity(intent);
+    }
+    public void logout(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
