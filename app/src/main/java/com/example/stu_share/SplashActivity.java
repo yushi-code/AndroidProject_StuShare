a1c7f7e70d428db630b9631f7f8a884afac80d18
David Shi <shy1860@hotmail.com>
2019-11-03 17:02:59 -0500

    open new branch to compare list joined events activity

--- app/src/main/java/com/example/stu_share/SplashActivity.java
+++ app/src/main/java/com/example/stu_share/SplashActivity.java
@@ -21,6 +21,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },1000);
+        },10000);
     }
 }
