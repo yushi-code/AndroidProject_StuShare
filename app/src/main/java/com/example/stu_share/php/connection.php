918b8787b7855590455d1fb50fd484e3d3833fdc
yushi-code <shy1860@hotmail.com>
2019-11-22 17:11:40 -0500

    clean github

--- app/src/main/java/com/example/stu_share/php/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "gblearn.com";
-$user_name = "f9team1_f9team1";
-$user_password = "Stushare127";
-$db_name = "f9team1_stushare";
-
-$con = mysqli_connect($host, $user_name,$user_password, $db_name);
-
-?>
