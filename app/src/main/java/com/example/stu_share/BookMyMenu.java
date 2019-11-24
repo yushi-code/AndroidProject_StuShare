0dcb9c1f198865c557337b3fde2cb8c75e40853d
yushi-code <shy1860@hotmail.com>
2019-11-24 17:47:17 -0500

    add activities in admin

--- app/src/main/java/com/example/stu_share/BookMyMenu.java
+++ app/src/main/java/com/example/stu_share/BookMyMenu.java
@@ -2,62 +2,13 @@ package com.example.stu_share;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class BookMyMenu extends AppCompatActivity {
-    Button btnHome, btnLogout, btnViewCurrent, btnViewPast, btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_my_menu);
-
-        btnHome=findViewById(R.id.btnHome);
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i= new Intent(getBaseContext(),MainMenu.class);
-                startActivity(i);
-            }
-        });
-
-        btnLogout=findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i= new Intent(getBaseContext(),MainActivity.class);
-                startActivity(i);
-            }
-        });
-
-        btnViewCurrent=findViewById(R.id.btnViewCurrentBook);
-        btnViewCurrent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getBaseContext(),BookCurrentList.class);
-                startActivity(i);
-            }
-        });
-
-        btnViewPast=findViewById(R.id.btnViewPastBook);
-        btnViewPast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getBaseContext(),BookPastList.class);
-                startActivity(i);
-            }
-        });
-
-        btnCreate=findViewById(R.id.createBook);
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getBaseContext(),BookCreate.class);
-                startActivity(i);
-            }
-        });
     }
 }
