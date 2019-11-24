2f85a2b04bb28fcffcfea75c7c481468c771695b
yushi-code <shy1860@hotmail.com>
2019-11-24 11:44:51 -0500

    add room category

--- app/src/main/java/com/example/stu_share/MainMenu.java
+++ app/src/main/java/com/example/stu_share/MainMenu.java
@@ -1,14 +1,11 @@
 package com.example.stu_share;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import static com.example.stu_share.CarCoordinator.CAR_LIST;
-import static com.example.stu_share.CarCoordinator.CAR_PAST_LIST;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenu extends AppCompatActivity {
     private Button buttonBooks,buttonRooms,buttonCars,buttonEvents,buttonMyProfile,buttonMessageCenter,buttonLogout,buttonContactUs;
@@ -32,9 +29,9 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-//                Intent i= new Intent(getBaseContext(),RoomMenu.class);
-//                i.putExtra("user",user);
-//                startActivity(i);
+                Intent i= new Intent(getBaseContext(),RoomMenu.class);
+                i.putExtra("user",user);
+                startActivity(i);
 
             }
         });
