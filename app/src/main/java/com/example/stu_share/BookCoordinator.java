2a16af7205bd525d2221b849aac9268d0bff8d83
Ayusha Chaudhry <ayusha.chaudhry@gmail.com>
2019-11-24 16:46:53 -0500

    so far

--- app/src/main/java/com/example/stu_share/BookCoordinator.java
+++ app/src/main/java/com/example/stu_share/BookCoordinator.java
@@ -1,9 +1,106 @@
 package com.example.stu_share;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookCoordinator {
     public final static List<Book> BOOK_LIST = new ArrayList<Book>();
     public final static List<Book> BOOK_PAST_LIST = new ArrayList<Book>();
+
+    public class Book implements Serializable {
+        public String id;
+        public String ownerId;
+        public String title;
+        public String author;
+        public String edition;
+        public String isbn;
+        public String price;
+        public String details;
+        public String created;
+        public String status;
+
+        public Book(String id, String ownerId, String title, String author, String edition, String isbn,
+                     String price, String details, String created, String status){
+            this.id = id;
+            this.ownerId = ownerId;
+            this.title = title;
+            this.author = author;
+            this.edition = edition;
+            this.isbn = isbn;
+            this.price = price;
+            this.details = details;
+            this.created = created;
+            this.status = status;
+        }
+
+        public String getId(){
+            return id;
+        }
+        public String getOwnerId(){
+            return ownerId;
+        }
+        public String getTitle(){
+            return title;
+        }
+        public String getAuthor(){
+            return author;
+        }
+        public String getEdition(){
+            return edition;
+        }
+        public String getIsbn(){
+            return isbn;
+        }
+        public String getPrice(){
+            return price;
+        }
+        public String getDetails(){
+            return details;
+        }
+        public String getCreated(){
+            return created;
+        }
+        public String getStatus(){
+            return status;
+        }
+
+
+        public void setId(String id) {
+            this.id = id;
+        }
+        public void setOwnerId(String ownerId) {
+            this.ownerId = ownerId;
+        }
+        public void setTitle(String title) {
+            this.title = title;
+        }
+        public void setAuthor(String author){
+            this.author = author;
+        }
+        public void setEdition(String edition) {
+            this.edition = edition;
+        }
+        public void setIsbn(String isbn) {
+            this.isbn = isbn;
+        }
+        public void setPrice(String price) {
+            this.price = price;
+        }
+        public void setDetails(String details) {
+            this.details = details;
+        }
+        public void setCreated(String created) {
+            this.created = created;
+        }
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+
+        public String toString(){
+            return "Title: " + title + "\nAuthor: " + author + "\nEdition: " + edition +
+                    "\nISBN: " + isbn +  "\nPrice" + price + "\nDetails: " + details;
+        }
+    }
 }
