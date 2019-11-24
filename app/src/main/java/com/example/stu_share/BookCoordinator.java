446c8e193744458c4a19a85e51a3b452f6da0b5e
Ayusha Chaudhry <ayusha.chaudhry@gmail.com>
2019-11-24 14:47:40 -0500

    BookMenu class completed

---
+++ app/src/main/java/com/example/stu_share/BookCoordinator.java
@@ -0,0 +1,9 @@
+package com.example.stu_share;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookCoordinator {
+    public final static List<Book> BOOK_LIST = new ArrayList<Book>();
+    public final static List<Book> BOOK_PAST_LIST = new ArrayList<Book>();
+}
--- app/src/main/java/com/example/stu_share/BookMenu.java
+++ app/src/main/java/com/example/stu_share/BookMenu.java
@@ -2,13 +2,75 @@ package com.example.stu_share;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BookMenu extends AppCompatActivity {
+    private Button btnMyBooks,btnViewBooks,btnCreateBook,btnMessage,btnHome,btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_menu);
+
+        btnMyBooks = findViewById(R.id.myBooks);
+        btnMyBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),BookMyMenu.class);
+                startActivity(i);
+            }
+        });
+
+
+        btnViewBooks = findViewById(R.id.viewBooks);
+        btnViewBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),BookList.class);
+                startActivity(i);
+            }
+        });
+
+
+        btnCreateBook = findViewById(R.id.createBooks);
+        btnCreateBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),BookCreate.class);
+                startActivity(i);
+            }
+        });
+
+        btnMessage = findViewById(R.id.messageCentre);
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),MessageListActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnHome=findViewById(R.id.home);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),MainMenu.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogout=findViewById(R.id.logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
