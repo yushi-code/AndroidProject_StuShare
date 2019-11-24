f62bed5d862926a9baf34bd80e628c9ccfb46f27
Ayusha Chaudhry <ayusha.chaudhry@gmail.com>
2019-11-24 17:16:18 -0500

    current book stuff updated

--- app/src/main/java/com/example/stu_share/BookCurrentDetail.java
+++ app/src/main/java/com/example/stu_share/BookCurrentDetail.java
@@ -18,6 +18,56 @@ public class BookCurrentDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_current_detail);
 
+        title=findViewById(R.id.txtCarTitle);
+        author=findViewById(R.id.txtCarDate);
+        edition=findViewById(R.id.txtCarBrand);
+        isbn=findViewById(R.id.txtCarModel);
+        price=findViewById(R.id.txtCarYear2);
+        description=findViewById(R.id.txtCarMileage);
 
+
+        final BookCoordinator.Book book=(BookCoordinator.Book)getIntent().getSerializableExtra("book");
+        final int position=(int)getIntent().getSerializableExtra("position");
+        terminate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BookCoordinator.BOOK_LIST.remove(book);
+                BookCoordinator.BOOK_PAST_LIST.add(book);
+                Toast.makeText(BookCurrentDetail.this,"Your car share has been terminated.",Toast.LENGTH_LONG).show();
+            }
+        });
+        home=findViewById(R.id.btnHome);
+        logout=findViewById(R.id.btnLogout);
+
+        update=findViewById(R.id.btnUpdate);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getBaseContext(),"Your information has been updated!",Toast.LENGTH_LONG);
+                Intent i=new Intent(getBaseContext(),CarMenu.class);
+                startActivity(i);
+            }
+        });
+
+        title.setText(book.title);
+        author.setText(book.author);
+        edition.setText(book.edition);
+        isbn.setText(book.isbn);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),MainMenu.class);
+                startActivity(i);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
