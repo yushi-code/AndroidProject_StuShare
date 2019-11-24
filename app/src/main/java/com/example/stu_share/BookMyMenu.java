3fe2977ddf55927b08e329b15db96f450e74aa99
Ayusha Chaudhry <ayusha.chaudhry@gmail.com>
2019-11-24 08:17:35 -0500

    creating all book activities

--- app/src/main/java/com/example/stu_share/BookMyMenu.java
+++ app/src/main/java/com/example/stu_share/BookMyMenu.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class bookMenu extends AppCompatActivity {
+public class BookMyMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_menu);
+        setContentView(R.layout.activity_book_my_menu);
     }
 }
