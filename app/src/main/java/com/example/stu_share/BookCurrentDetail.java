732814d788d223cb6fb94e1c1f212c3685e10cad
Ayusha Chaudhry <ayusha.chaudhry@gmail.com>
2019-11-24 19:49:10 -0500

    price in current details book added

--- app/src/main/java/com/example/stu_share/BookCurrentDetail.java
+++ app/src/main/java/com/example/stu_share/BookCurrentDetail.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class BookCurrentDetail extends AppCompatActivity {
 
-    private EditText title, author, edition, isbn, details;
+    private EditText title, author, edition, isbn, details, price;
     private Button home,logout,terminate,update;
 
 
@@ -25,6 +25,10 @@ public class BookCurrentDetail extends AppCompatActivity {
         edition=findViewById(R.id.txtEdition);
         isbn=findViewById(R.id.txtISBN);
         details=findViewById(R.id.txtDetail);
+        price=findViewById(R.id.txtPrice);
+
+        terminate=findViewById(R.id.btnTerminate);
+
 
 
         final BookCoordinator.Book book=(BookCoordinator.Book)getIntent().getSerializableExtra("book");
@@ -56,6 +60,7 @@ public class BookCurrentDetail extends AppCompatActivity {
         edition.setText(book.edition);
         isbn.setText(book.isbn);
         details.setText(book.details);
+        price.setText(book.price);
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
